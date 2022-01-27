b4962ce04c75017f257006f6e8c3139c383352a9
sait.ekmekcibasi <Z14Bmck36>
2022-01-27 14:54:36 +0200

    Admin and User Authentication

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\User\UserDashboardComponent;
+use App\Http\Livewire\Admin\AdminDashboardComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,6 +31,16 @@ Route::get('/cart',CartComponent::class);
 
 Route::get('/checkout',CheckoutComponent::class);
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+/*Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
-})->name('dashboard');
+})->name('dashboard');*/
+
+// For User ou Customer
+Route::middleware(['auth:sanctum', 'verified'])->group(function (){
+    Route::get('/user/dashboard', UserDashboardComponent::class)->name('user.dashboard');
+});
+
+// For Admin
+Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function (){
+    Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+});
