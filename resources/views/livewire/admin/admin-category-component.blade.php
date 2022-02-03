108b6536ca38376d06580666fabe281f2861794a
sait.ekmekcibasi <Z14Bmck36>
2022-02-03 10:47:49 +0200

    Admin Add New Category

--- resources/views/livewire/admin/admin-category-component.blade.php
+++ resources/views/livewire/admin/admin-category-component.blade.php
@@ -13,7 +13,15 @@
             <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        All Categories
+                        <div class="row">
+                            <div class="col-md-6">
+                                Add New Category
+                            </div>
+                            <div class="col-md-6">
+                                <a href="{{route('admin.addcategory')}}" class="btn btn-success pull-right">Add New
+                                    Category</a>
+                            </div>
+                        </div>
                     </div>
                     <div class="panel-body">
                         <table class="table table-striped">
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
+use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\DetailsComponent;
@@ -54,4 +55,5 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function (){
     Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
     Route::get('/admin/categories', AdminCategoryComponent::class)->name('admin.categories');
+    Route::get('/admin/category/add', AdminAddCategoryComponent::class)->name('admin.addcategory');
 });
