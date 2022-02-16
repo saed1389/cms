9deaa146845385d59063075adfc764d5f1c4909e
sait.ekmekcibasi <Z14Bmck36>
2022-02-16 09:43:05 +0200

    Admin Create Coupons

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -124,6 +124,10 @@
                                                        title="Manage Home Categories">Sale Setting</a>
                                                 </li>
                                                 <li class="menu-item">
+                                                    <a href="{{route('admin.coupons')}}"
+                                                       title="Manage Home Categories">All Coupons</a>
+                                                </li>
+                                                <li class="menu-item">
                                                     <a href="{{ route('logout') }}"
                                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
                                                 </li>
