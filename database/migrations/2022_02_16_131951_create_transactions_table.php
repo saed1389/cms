2d3293d236fd96f43295a67e79f08044aaf0e20d
sait.ekmekcibasi <Z14Bmck36>
2022-02-17 11:40:31 +0200

    Admin Create Model, Migration and Relationship for Checkout

---
+++ database/migrations/2022_02_16_131951_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('product_id')->unsigned();
+            $table->enum('mode', [ 'cod', 'card', 'paypal' ]);
+            $table->enum('status', [ 'pending', 'approved', 'declined', 'refunded' ])->default('pending');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
